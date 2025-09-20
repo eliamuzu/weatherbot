0d91b1fd94a565b701dbb30ef9f043abbf7fc32e
eliamuzu <kofiprince375@gmail.com>
2025-09-20 22:16:02 +0000

    added twitter integration using tweepy

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from pyowm.owm import OWM
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import tweepy
 
+# loading all environment variables needed
 load_dotenv()
-
 API_KEY = os.getenv('OWM_API_KEY')
-BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
+API_SECRET =  os.getenv("TWITTER_CONSUMER_SECRET")
+ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
+TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
+
+#authentication for twitter api using tweepy
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
+#Weather condition generaation
 location = input("What city are you in? ")
 today = datetime.now()
 
@@ -23,12 +33,14 @@ try :
     weather_humidity = w.humidity
     weather_temp = w.temperature('celsius')
 
-    print(
-        f" \n"
-        f"Good morning! Welcome to your local weather forecast for {today}.\n"
-        f"We are starting the day off with {weather_status} and a temperature of {weather_temp['temp']} degrees.\n"
-        f"It feels like {weather_temp['feels_like']} outside.\n" 
-        f"We have winds moving at {weather_wind['speed']} mph\n"
-) 
+    
+    current_weather = f" \n. Good morning! Welcome to your local weather forecast for {today}.\n We are starting the day off with {weather_status} and a temperature of {weather_temp['temp']} degrees.\n It feels like {weather_temp['feels_like']} outside.\nWe have winds moving at {weather_wind['speed']} mph\n"
+
+except Exception as e:
+    print(f"Error: {e}")
+
+
+try:
+    api.update_status(current_weather)
 except Exception as e:
     print(f"Error: {e}")

f6241ec55119722903e9df7c48d3f132e5f41ac3
eliamuzu <peamuzu003@st.ug.edu.gh>
2025-09-23 13:48:52 +0000

    changes to twitter api wrapper

--- main.py
+++ main.py
@@ -2,45 +2,43 @@ from pyowm.owm import OWM
 from datetime import datetime
 from dotenv import load_dotenv
 import os
-import tweepy
+import twitter
 
 # loading all environment variables needed
 load_dotenv()
-API_KEY = os.getenv('OWM_API_KEY')
-API_SECRET =  os.getenv("TWITTER_CONSUMER_SECRET")
+OWM_KEY = os.getenv('OWM_API_KEY')
+API_KEY = os.getenv("TWITTER_API_KEY")
+API_KEY_SECRET =  os.getenv("TWITTER_API_KEY_SECRET")
 ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 LOCATION = os.getenv('WEATHER_LOCATION', 'Accra')
 
 
 #authentication for twitter api using tweepy
-auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, TOKEN_SECRET)
-api = tweepy.API(auth)
+api = twitter.Api(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, TOKEN_SECRET)
+today = datetime.now()
+
+try :
+    owm =  OWM(OWM_KEY)
+    weather_manager = owm.weather_manager()
+    observation = weather_manager.weather_at_place(LOCATION)
+    w = observation.weather
 
 
-def post_weather():
-    today = datetime.now()
-    try :
-        owm =  OWM(API_KEY)
-        weather_manager = owm.weather_manager()
-        observation = weather_manager.weather_at_place(LOCATION)
-        w = observation.weather
+    weather_status = w.detailed_status
+    weather_wind = w.wind()
+    weather_humidity = w.humidity
+    weather_temp = w.temperature('celsius')
 
-
-        weather_status = w.detailed_status
-        weather_wind = w.wind()
-        weather_humidity = w.humidity
-        weather_temp = w.temperature('celsius')
-
+    
+    current_weather = (
+        f"Good morning! Weather forecast for {LOCATION} at {today:%Y-%m-%d %H:%M}.\n"
+        f"{weather_status}, {weather_temp['temp']}°C (feels like {weather_temp['feels_like']}°C).\n"
+        f"Humidity: {weather_humidity}%. Wind: {weather_wind['speed']} mph."
+    )     
         
-        current_weather = (
-            f"Good morning! Weather forecast for {LOCATION} at {today:%Y-%m-%d %H:%M}.\n"
-            f"{weather_status}, {weather_temp['temp']}°C (feels like {weather_temp['feels_like']}°C).\n"
-            f"Humidity: {weather_humidity}%. Wind: {weather_wind['speed']} mph."
-        )     
-           
-        api.update_status(current_weather)
+    api.PostUpdate(current_weather)
+    print("tweet posted")
 
-    except Exception as e:
-        print(f"Error: {e}")
-
+except Exception as e:
+    print(f"Error: {e}")
